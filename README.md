fed5f7f94ce465abaa024152e257122f6e7cfb94
vascofg <vascofg@gmail.com>
2015-05-19 01:17:10 +0100

    Switched to more modern config using jetty

--- README.md
+++ README.md
@@ -1,28 +1,12 @@
-## JAX-RS with Grizzly
+## JAX-RS with Jetty
 
-JAX-RS is a great way to build RESTful services in Java. This is a very quick and simple example of a REST service.
+Using tutorial from [Jersey documentation](https://jersey.java.net/documentation/latest/getting-started.html#heroku-webapp).
 
 ## Build and Run
 
 Build the code with:
 
-    $ mvn package
-
-The POM file uses the [appassembler plugin](http://mojo.codehaus.org/appassembler/appassembler-maven-plugin/) to generate a wrapper script, so it's very simple to run the app. Simply execute:
-
-    $ sh target/bin/app
-    Starting grizzly...
-    Oct 25, 2011 10:06:44 AM com.sun.grizzly.Controller logVersion
-    INFO: Starting Grizzly Framework 1.9.18-i - Tue Oct 25 10:06:44 PDT 2011
-    Jersey started with WADL available at http://localhost:9998/application.wadl.
-
-(On Windows use target/bin/app.bat instead)
-
-## WADL
-
-A .WADL file describes the resources provided by a service and the relationships between them.
-
-After running the app the WADL file will be available at */application.wadl.
+    $ mvn clean package
 
 ## Heroku
 
--- src/main/java/org/tnel/meau/Meau.java
+++ src/main/java/org/tnel/meau/Meau.java
@@ -1,61 +1,78 @@
 package org.tnel.meau;
 
-import com.sun.grizzly.http.SelectorThread;
-import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
-import jade.wrapper.StaleProxyException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.tnel.meau.items.Product;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
+ * command that is used for launching should fire this main method.
+ */
 public class Meau {
 
     public static List<Product> products= new LinkedList<Product>();
     public static jade.wrapper.AgentContainer mainContainer = null;
 
-    public static void main(String[] args) throws IOException, StaleProxyException {
-        products.add(new Product(1, "Batatas vermelhas", "Batatas ideais para fritar", 20));
-        products.add(new Product(2,"Batatas brancas","Batatas ideais para cozer", 18));
-        
-        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
-        final Map<String, String> initParams = new HashMap<String, String>();
-        initParams.put("com.sun.jersey.config.property.packages","org.tnel.meau.resources");
+    public static void main(String[] args) throws Exception{
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
 
-        System.out.println("Starting grizzly...");
-        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-        System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
+        final Server server = new Server(Integer.valueOf(webPort));
+        final WebAppContext root = new WebAppContext();
+
+        root.setContextPath("/");
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
+
+        final String webappDirLocation = "src/main/webapp/";
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
+        server.setHandler(root);
+
+        // Add sample data
+        products.add(new Product(1, "Batatas vermelhas", "Batatas ideais para fritar", 20));
+        products.add(new Product(2, "Batatas brancas", "Batatas ideais para cozer", 18));
+
+        // Start JADE
 
         // Get a hold on JADE runtime
         jade.core.Runtime rt = jade.core.Runtime.instance();
 
         // Exit the JVM when there are no more containers around
         rt.setCloseVM(true);
-        System.out.print("[JADE]runtime created\n");
 
         // Create a default profile
         Profile profile = new ProfileImpl(null, 1200, null);
-        System.out.print("[JADE]profile created\n");
 
-        System.out.println("[JADE]Launching a whole in-process platform..."+profile);
         mainContainer = rt.createMainContainer(profile);
 
         // now set the default Profile to start a container
         ProfileImpl pContainer = new ProfileImpl(null, 1200, null);
-        System.out.println("[JADE]Launching the agent container ..."+pContainer);
 
         jade.wrapper.AgentContainer cont = rt.createAgentContainer(pContainer);
-        System.out.println("[JADE]Launching the agent container after ..."+pContainer);
 
-        System.out.println("[JADE]containers created");
         /*System.out.println("[JADE]Launching the rma agent on the main container ...");
         AgentController rma = mainContainer.createNewAgent("rma",
                 "jade.tools.rma.rma", new Object[0]);
         rma.start();*/
 
+
+        // Start server
+        server.start();
+        server.join();
     }
 }
